332423153d7f5ad53af8bb3f98866ae40a88ec70
unknown <seb111.penna@gmail.com>
2019-10-15 21:57:59 +0200

    add render hiddenLetter function

---
+++ src/Components/HiddenLetter.jsx
@@ -0,0 +1,18 @@
+import React,{Fragment,Component} from 'react';
+import '../App.css';
+
+
+class HiddenLetter extends Component{
+
+
+
+    render(){
+        return
+    }
+
+}
+
+
+
+
+export default HiddenLetter
--- src/Pages/GameSession.jsx
+++ src/Pages/GameSession.jsx
@@ -1,15 +1,22 @@
 import React,{Fragment,Component} from 'react';
 import '../App.css';
-
+import HiddenLetter from '../Components/HiddenLetter'
 
 const sentences = ["Javascript is cool","You guessed right","React is a framework from Facebook","This is made with React"]
 
 class GameSession extends Component {
 
  debugger = () => {
-    console.log(this.props.history)
+    console.log(this.props.history,this.props.location.state.randomIndex)
 }
 
+hiddenLetterRenderer = () => {
+let randomSentence = this.props.location.state.randomIndex
+sentences[randomSentence].map((letter,index) =>{
+return <HiddenLetter hiddenCharacter={letter}></HiddenLetter>
+})
+
+}
     render(){
         return(<Fragment>
             <h1>GameSession</h1>
