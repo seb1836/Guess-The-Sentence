5d047aa68ba8989d11632889b494b9eb1a7a768f
unknown <seb111.penna@gmail.com>
2019-10-16 18:28:07 +0200

    add the core of the game

--- src/Components/HiddenLetter.jsx
+++ src/Components/HiddenLetter.jsx
@@ -2,12 +2,29 @@ import React,{Fragment,Component} from 'react';
 import '../App.css';
 
 
-class HiddenLetter extends Component{
+class HiddenLetter extends Component {
 
+    debug = () => {
+        console.log("debug")
+    }
+checkIfEmptyChar = () => {
 
+    if (this.props.hiddenCharacter === " "){
+        return <span></span>
+    }return this.props.hiddenCharacter
+}
+
+hideOrDisplayLetter = () => {
+
+}
 
     render(){
-        return
+        return(
+        <div className="letter">{this.props.hiddenCharacter}
+        
+        {this.debug()}
+        </div>
+        )
     }
 
 }
--- src/Pages/GameSession.jsx
+++ src/Pages/GameSession.jsx
@@ -3,8 +3,13 @@ import '../App.css';
 import HiddenLetter from '../Components/HiddenLetter'
 
 const sentences = ["Javascript is cool","You guessed right","React is a framework from Facebook","This is made with React"]
+const keyboard = ['q','w','e','r','t','y','u','i','o','p','','a','s','d','f','g','h','j','k','l','','z','x','c','v','b','n','m']
+let count = 0;
 
 class GameSession extends Component {
+ state={
+     keyClicked:"none"
+ }   
 
  debugger = () => {
     console.log(this.props.history,this.props.location.state.randomIndex)
@@ -12,15 +17,41 @@ class GameSession extends Component {
 
 hiddenLetterRenderer = () => {
 let randomSentence = this.props.location.state.randomIndex
-sentences[randomSentence].map((letter,index) =>{
-return <HiddenLetter hiddenCharacter={letter}></HiddenLetter>
+let chars = sentences[randomSentence].split('')
+ return chars.map((letter,index) =>{
+        console.log(sentences[randomSentence].charAt(index),sentences[randomSentence],letter,chars)
+        if (letter === this.state.keyClicked){
+            return <HiddenLetter hiddenCharacter={letter}></HiddenLetter>
+        }else if (letter === " "){
+            return <span></span>
+        }
+        return <HiddenLetter hiddenCharacter={" "} ></HiddenLetter>
+
+
 })
 
+}
+
+compareKeyWithSentence = (event) => {
+console.log(event.target.innerHTML)
+this.setState({keyClicked:event.target.innerHTML})
+}
+
+keyRenderer = () => {
+    return keyboard.map((key,index) => {
+        count ++
+        if (key===''){
+            return <br/>
+        }
+        return <span id={count} onClick={this.compareKeyWithSentence}>{key}</span>
+    })
 }
     render(){
         return(<Fragment>
             <h1>GameSession</h1>
             {this.debugger()}
+            <div className="sentence">{this.hiddenLetterRenderer()}</div>
+            <div className="keyRenderer">{this.keyRenderer()}</div>
             </Fragment>
         )
         
